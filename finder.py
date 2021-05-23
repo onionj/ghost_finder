7896d17ba68a2dfeefc51120f7f8bd54401211a9
onionj98 <onionj98@gmail.com>
2021-05-24 02:05:18 +0430

    bild Finder class

---
+++ finder.py
@@ -0,0 +1,15 @@
+
+
+class Finder:
+    def __init__(self, username, password) -> None:
+        self.username = username
+        self.password = password
+
+    def pepole_not_liker_us(self):
+        pass
+
+    def pepole_have_lot_of_following(self):
+        pass
+
+    def pepole_not_have_profile(self):
+        pass
