cd4338f5173f04bff27d6715d577ceb4b308b245
onionj98 <onionj98@gmail.com>
2021-05-24 02:55:08 +0430

    edit __init__

--- finder.py
+++ finder.py
@@ -4,12 +4,23 @@ class Finder:
     def __init__(self, username, password) -> None:
         self.username = username
         self.password = password
+        self.our_follower = []
+        self.pepole_not_like_us = []
+        self.pepole_have_lot_of_following = []
+        self.pepole_not_have_profile = []
+        self.white_paper = []
 
-    def pepole_not_like_us(self):
+    def get_our_follower(self):
         pass
 
-    def pepole_have_lot_of_following(self):
+    def get_pepole_not_like_us(self):
         pass
 
-    def pepole_not_have_profile(self):
+    def get_pepole_have_lot_of_following(self):
+        pass
+
+    def get_pepole_not_have_profile(self):
+        pass
+
+    def remove_pepole(self, pepole, delay):
         pass
